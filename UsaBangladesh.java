cf24eb7f59bac603ac24a881033e36d04f47a080
Mohammad Siddiqullah <msiddiiq48@gmail.com>
2026-08-05 17:29:33 -0400

    Add UsaBangladesh Java program

---
+++ UsaBangladesh.java
@@ -0,0 +1,9 @@
+public class UsaBangladesh
+{
+    public static void main (String [] args)
+ {
+
+	System.out.println("Usa Bangladesh Friendship");
+	System.out.println("Proof: US-Bangla Airlines");
+ }
+}
